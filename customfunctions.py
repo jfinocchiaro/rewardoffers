bcb4e0d8ed339c83e2dde4bbee030d2c3a5bf5f0
jfinocchiaro <22jjf22@gmail.com>
2017-04-30 20:30:17 -0400

    Transfer from previous project

---
+++ customfunctions.py
@@ -0,0 +1,10 @@
+
+#evaluate score maximizing financial gains and minimizing missed oppotunities
+def evaluate(member):
+    score1 = 0
+    score2 = 0
+    if spotsLeft = 0:
+        score2 += 1
+    spotsLeft = ''.join(member[0][64:66])
+    print spotsLeft
+    return score1, score2
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from deap import tools, base, creator, algorithms
 import customfunctions
 import itertools
+import math
 
 def main():
     #multiobjective- maximize reward, minimize missed opportunities
@@ -10,28 +11,47 @@ def main():
     creator.create("Individual", list, fitness=creator.FitnessMulti)
 
 
-    IND_SIZE = 70    #length of genome
-    POP_SIZE = 150   #number of members in the population
-
+    IND_SIZE = 8     #length of genome
+    POP_SIZE = 120    #number of members in the population
+    DECISION_SIZE = 3 #four decisions can be made
 
     toolbox = base.Toolbox()    #initialize toolbox
-    toolbox.register("attr_int", random.randint, 0 , 0) #initialize to 0
-
-
-
+    toolbox.register("initZero", random.randint, 0 , 0) #initialize to 0
     toolbox.register("bit", random.randint, 0, 1) #create a bit 0 or 1
-    toolbox.register("genome", tools.initRepeat, list, toolbox.bit, IND_SIZE) #list of bits makes up genome
-    toolbox.register("individual", tools.initCycle, creator.Individual, (toolbox.genome, toolbox.attr_int,toolbox.attr_int,toolbox.attr_int,toolbox.attr_int, toolbox.attr_int), n=1)    #creates an individual with genome and scores
+    toolbox.register("decision", random.randint, 0, 3) #create a bit 0 to 3
+
+    toolbox.register("genome", tools.initRepeat, list, toolbox.decision, IND_SIZE) #list of bits makes up genome
+    toolbox.register("offerAmts", tools.initRepeat, list, toolbox.bit, DECISION_SIZE) #list of bits makes up genome
+    toolbox.register("scores", tools.initRepeat, list, toolbox.initZero, 2) #list of bits makes up genome
+
+    toolbox.register("individual", tools.initCycle, creator.Individual, (toolbox.genome, toolbox.offerAmts, toolbox.scores), n=1)    #creates an individual with genome and scores
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)  #list of individuals makes up population
     population = toolbox.population(n=POP_SIZE) #initialize population
 
     #initialize evolution methods
-    toolbox.register("evaluate", deapplaygame.evaluate)
+    toolbox.register("evaluate", customfunctions.evaluate)
     toolbox.register("mate", tools.cxOnePoint)
-    toolbox.register("mutate", )
+    toolbox.register("mutate", tools.mutShuffleIndexes)
     toolbox.register("select", tools.selNSGA2)
 
     NGEN = 5000 #number of generations of evolution
+    CXPB = (0.9)
+    MUTPB = (0.1)
+
+
+    #3 bits for offers remaining, 3 bits for offer amount (values in [500, 1000, 2000, 3000, 4000, 5000, 7500, 10000])
+    for gen in range(NGEN):
+        offersLeft = random.randint(2, 16)
+        random.shuffle(population)
+        while(offersLeft > 0):
+            for member in population:
+                toolbox.evaluate
+
+        offspring = algorithms.varAnd(population, toolbox, cxpb=CXPB, mutpb=MUTPB)
+        fits = toolbox.map(toolbox.evaluate, offspring)
+        for fit, ind in zip(fits, offspring):
+            ind.fitness.values = fit
+        population = offspring
 
 
 if __name__ == "__main__":
